53c2e0363b658735a57f151a8a20230bfd409ef1
jyryuitpro <jy.ryu.itpro@gmail.com>
2021-10-28 21:33:53 +0900

    Scaffolding a Flutter App

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,17 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
-      home: Center(
-        child: Text('Hello World'),
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey,
+        appBar: AppBar(
+          title: Text('I Am Rich'),
+          backgroundColor: Colors.blueGrey[900],
+        ),
+        body: Center(
+          child: Image(
+            image: NetworkImage('http://www.w3bai.com/w3css/img_lights.jpg'),
+          ),
+        ),
       ),
     ),
   );
